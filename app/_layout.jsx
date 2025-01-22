ae3607c3e197f803c362f4d2c561591f6836adca
Whisperer <whisperer.meta@gmail.com>
2025-01-22 19:51:38 +0200

    Finished Authentication

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,8 +1,36 @@
 import { View, Text } from 'react-native'
-import React from 'react'
-import { Stack } from 'expo-router'
+import React, { useEffect } from 'react'
+import { Stack, useRouter } from 'expo-router'
+import { AuthProvider, useAuth } from '../contexts/AuthContext'
+import { supabase } from '../lib/superbase'
 
-const _layout = () => {
+const _layout = ()=> {
+  return (
+    <AuthProvider>
+      <MainLayout/>
+    </AuthProvider>
+  )
+}
+
+const MainLayout = () => {
+  const {setAuth} = useAuth();
+  const router = useRouter();
+  useEffect(()=>{
+    supabase.auth.onAuthStateChange((_event, session) => {
+      if (session) {
+        // Set Auth
+        setAuth(session?.user);
+        router.replace('/home');
+
+        // Move to home screen
+      } else {
+        // Set auth as null
+        setAuth(null);
+        // Move to welcome screen
+        router.replace('/welcome');
+      }
+    })
+  }, [])
   return (
     <Stack 
         screenOptions={{

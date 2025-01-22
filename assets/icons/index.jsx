4610864dac2ad7838dfd8b5f2493097a88f4c593
Whisperer <whisperer.meta@gmail.com>
2025-01-22 17:17:51 +0200

    Added all icons to Icon manager

--- assets/icons/index.jsx
+++ assets/icons/index.jsx
@@ -2,12 +2,50 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { theme } from '../../constants/theme';
 
-import HomeIcon from './home'
+import ArrowLeftIcon from './arrowLeft'
+import CallIcon from './call'
+import CameraIcon from './camera'
+import CommentIcon from './comment'
+import DeleteIcon from './delete'
+import EditIcon from './edit'
 import EmailIcon from './email'
+import HeartIcon from './heart'
+import HomeIcon from './home'
+import ImageIcon from './image'
+import LocationIcon from './location'
+import LockIcon from './lock'
+import LogoutIcon from './logout'
+import PlusIcon from './plus'
+import SearchIcon from './search'
+import SendIcon from './send'
+import ShareIcon from './share'
+import ThreeDotsCircleIcon from './threeDotsCircle'
+import ThreeDotsHorizontalIcon from './threeDotsHorizontal'
+import UserIcon from './user'
+import VideoIcon from './video'
 
 const icons = {
-    home: HomeIcon,
-    email: EmailIcon,
+  arrowLeft: ArrowLeftIcon,
+  call: CallIcon,
+  camera: CameraIcon,
+  comment: CommentIcon,
+  delete: DeleteIcon,
+  edit: EditIcon,
+  email: EmailIcon,
+  heart: HeartIcon,
+  home: HomeIcon,
+  image: ImageIcon,
+  location: LocationIcon,
+  lock: LockIcon,
+  logout: LogoutIcon,
+  plus: PlusIcon,
+  search: SearchIcon,
+  send: SendIcon,
+  share: ShareIcon,
+  threeDotsCircle: ThreeDotsCircleIcon,
+  threeDotsHorizontal: ThreeDotsHorizontalIcon,
+  user: UserIcon,
+  video: VideoIcon,
 }
 
 const Icon = ({name, ...props}) => {
